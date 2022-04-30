055b4ecd529fdaea5a6bdcca7e612a7c24abfc9c
Moises Godoy <moigodoy@gmail.com.com>
2022-04-29 21:27:04 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,309 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
+
+
+function valida_envia(){
+    //valido el rut
+    if (document.fvalida.rut.value.length==0){
+           alert("Tiene que escribir su Rut")
+           document.fvalida.rut.focus()
+           return 0;
+    }
+
+    //valido el digito
+    if (document.fvalida.fdigit.value.length==0){
+        alert("Tiene que escribir su Digito Verificador")
+        document.fvalida.fdigit.focus()
+        return 0
+     }else{
+          const digive = dgv(fvalida.rut.value) 
+        if (fvalida.fdigit.value!=digive){
+            alert("rut incorrecto")
+            document.fvalida.fdigit.focus()
+            return 0
+        }
+     }
+
+    //valido el nombre
+    if (document.fvalida.name.value.length==0){
+        alert("Tiene que escribir su Nombre")
+        document.fvalida.name.focus()
+        return 0;
+ }
+ 
+     //valido el apellido
+     if (document.fvalida.lastname.value.length==0){
+        alert("Tiene que escribir su Apellido")
+        document.fvalida.lastname.focus()
+        return 0;
+ }
+
+    //valido la direccion
+    if (document.fvalida.adress.value.length==0){
+        alert("Tiene que escribir su Dirección")
+        document.fvalida.adress.focus()
+        return 0;
+ }
+
+    //valido la region
+    if (document.fvalida.regiones.selectedIndex==0){
+        alert("Tiene que seleccionar una Region")
+        document.fvalida.comuna.focus()
+        return 0;
+ }
+
+    //valido la comuna
+    if (document.fvalida.comunas.selectedIndex==0){
+        alert("Tiene que seleccionar una Comuna")
+        document.fvalida.comuna.focus()
+        return 0;
+ }
+
+    //valido el tipo de vehiculo
+    if (document.fvalida.typecar.selectedIndex==0){
+        alert("Debe seleccionar un tipo de Vehiculo.")
+        document.fvalida.typecar.focus()
+        return 0;
+ }
+
+    //valido la marca
+    if (document.fvalida.typemarca.selectedIndex==0){
+        alert("Tiene que escribir la marca de su vehiculo")
+        document.fvalida.marca.focus()
+        return 0;
+ }
+
+    //valido el modelo
+    if (document.fvalida.modelo.value.length==0){
+        alert("Tiene que escribir el Modelo de su Vehiculo")
+        document.fvalida.modelo.focus()
+        return 0;
+ }
+
+    //valido el año
+    if (document.fvalida.typeyear.selectedIndex==0){
+        alert("Tiene que seleccionar el Año de su Vehiculo")
+        document.fvalida.year.focus()
+        return 0;
+ }
+
+    //valido revision tecnica
+    if (document.fvalida.typertecnica.selectedIndex==0){
+        alert("Debe seleccionar revision tecnica.")
+        document.fvalida.rtecnica.focus()
+        return 0;0
+ }
+
+     //valido lavado
+    if (document.fvalida.typetiposer.selectedIndex==0){
+        alert("Tiene que seleccionar un tipo de servicio")
+        document.fvalida.flexRadioDefault1.focus()
+        return 0;
+ }
+
+    //valido encargado
+    if (document.fvalida.Encargado.selectedIndex==0){
+        alert("Debe seleccionar Encargado.")
+        document.fvalida.Encargado.focus()
+        return 0;0
+ }
+
+    //valido fecha
+    if (document.fvalida.fecha.selectedIndex==0){
+        alert("Debe seleccionar Fecha.")
+        document.fvalida.fecha.focus()
+        return 0;0
+ }
+
+    //valido hora
+    if (document.fvalida.hora.selectedIndex==0){
+        alert("Debe seleccionar Encargado.")
+        document.fvalida.hora.focus()
+        return 0;0
+ }
+
+ 
+    //el formulario se envia
+
+ ///   alert(document.fvalida.typetiposer.selectedIndex);
+
+    if (document.fvalida.typetiposer.selectedIndex==3){
+        var total = Math.round((7000 + 15000)*1.19)
+    }else if(document.fvalida.typetiposer.selectedIndex==1){
+        var total = Math.round(7000*1.19)
+    }else if(document.fvalida.typetiposer.selectedIndex==2){
+        var total = Math.round(15000*1.19)
+    }
+
+    alert("Muchas gracias por enviar el formulario" + "El Valor total de su servicio es : "  + total);
+    document.fvalida.submit();
+}
+
+function dgv(T)    //digito verificador
+{  
+      var M=0,S=1;
+	  for(;T;T=Math.floor(T/10))
+      S=(S+T%10*(9-M++%6))%11;
+        return S?S-1:'k';
+      
+//      alert(S?S-1:'k');
+ }
+
+ var RegionesYcomunas = {
+
+	"regiones": [{
+			"NombreRegion": "Arica y Parinacota",
+			"comunas": ["Arica", "Camarones", "Putre", "General Lagos"]
+	},
+		{
+			"NombreRegion": "Tarapacá",
+			"comunas": ["Iquique", "Alto Hospicio", "Pozo Almonte", "Camiña", "Colchane", "Huara", "Pica"]
+	},
+		{
+			"NombreRegion": "Antofagasta",
+			"comunas": ["Antofagasta", "Mejillones", "Sierra Gorda", "Taltal", "Calama", "Ollagüe", "San Pedro de Atacama", "Tocopilla", "María Elena"]
+	},
+		{
+			"NombreRegion": "Atacama",
+			"comunas": ["Copiapó", "Caldera", "Tierra Amarilla", "Chañaral", "Diego de Almagro", "Vallenar", "Alto del Carmen", "Freirina", "Huasco"]
+	},
+		{
+			"NombreRegion": "Coquimbo",
+			"comunas": ["La Serena", "Coquimbo", "Andacollo", "La Higuera", "Paiguano", "Vicuña", "Illapel", "Canela", "Los Vilos", "Salamanca", "Ovalle", "Combarbalá", "Monte Patria", "Punitaqui", "Río Hurtado"]
+	},
+		{
+			"NombreRegion": "Valparaíso",
+			"comunas": ["Valparaíso", "Casablanca", "Concón", "Juan Fernández", "Puchuncaví", "Quintero", "Viña del Mar", "Isla de Pascua", "Los Andes", "Calle Larga", "Rinconada", "San Esteban", "La Ligua", "Cabildo", "Papudo", "Petorca", "Zapallar", "Quillota", "Calera", "Hijuelas", "La Cruz", "Nogales", "San Antonio", "Algarrobo", "Cartagena", "El Quisco", "El Tabo", "Santo Domingo", "San Felipe", "Catemu", "Llaillay", "Panquehue", "Putaendo", "Santa María", "Quilpué", "Limache", "Olmué", "Villa Alemana"]
+	},
+		{
+			"NombreRegion": "Región del Libertador Gral. Bernardo O’Higgins",
+			"comunas": ["Rancagua", "Codegua", "Coinco", "Coltauco", "Doñihue", "Graneros", "Las Cabras", "Machalí", "Malloa", "Mostazal", "Olivar", "Peumo", "Pichidegua", "Quinta de Tilcoco", "Rengo", "Requínoa", "San Vicente", "Pichilemu", "La Estrella", "Litueche", "Marchihue", "Navidad", "Paredones", "San Fernando", "Chépica", "Chimbarongo", "Lolol", "Nancagua", "Palmilla", "Peralillo", "Placilla", "Pumanque", "Santa Cruz"]
+	},
+		{
+			"NombreRegion": "Región del Maule",
+			"comunas": ["Talca", "ConsVtución", "Curepto", "Empedrado", "Maule", "Pelarco", "Pencahue", "Río Claro", "San Clemente", "San Rafael", "Cauquenes", "Chanco", "Pelluhue", "Curicó", "Hualañé", "Licantén", "Molina", "Rauco", "Romeral", "Sagrada Familia", "Teno", "Vichuquén", "Linares", "Colbún", "Longaví", "Parral", "ReVro", "San Javier", "Villa Alegre", "Yerbas Buenas"]
+	},
+		{
+			"NombreRegion": "Región del Biobío",
+			"comunas": ["Concepción", "Coronel", "Chiguayante", "Florida", "Hualqui", "Lota", "Penco", "San Pedro de la Paz", "Santa Juana", "Talcahuano", "Tomé", "Hualpén", "Lebu", "Arauco", "Cañete", "Contulmo", "Curanilahue", "Los Álamos", "Tirúa", "Los Ángeles", "Antuco", "Cabrero", "Laja", "Mulchén", "Nacimiento", "Negrete", "Quilaco", "Quilleco", "San Rosendo", "Santa Bárbara", "Tucapel", "Yumbel", "Alto Biobío", "Chillán", "Bulnes", "Cobquecura", "Coelemu", "Coihueco", "Chillán Viejo", "El Carmen", "Ninhue", "Ñiquén", "Pemuco", "Pinto", "Portezuelo", "Quillón", "Quirihue", "Ránquil", "San Carlos", "San Fabián", "San Ignacio", "San Nicolás", "Treguaco", "Yungay"]
+	},
+		{
+			"NombreRegion": "Región de la Araucanía",
+			"comunas": ["Temuco", "Carahue", "Cunco", "Curarrehue", "Freire", "Galvarino", "Gorbea", "Lautaro", "Loncoche", "Melipeuco", "Nueva Imperial", "Padre las Casas", "Perquenco", "Pitrufquén", "Pucón", "Saavedra", "Teodoro Schmidt", "Toltén", "Vilcún", "Villarrica", "Cholchol", "Angol", "Collipulli", "Curacautín", "Ercilla", "Lonquimay", "Los Sauces", "Lumaco", "Purén", "Renaico", "Traiguén", "Victoria", ]
+	},
+		{
+			"NombreRegion": "Región de Los Ríos",
+			"comunas": ["Valdivia", "Corral", "Lanco", "Los Lagos", "Máfil", "Mariquina", "Paillaco", "Panguipulli", "La Unión", "Futrono", "Lago Ranco", "Río Bueno"]
+	},
+		{
+			"NombreRegion": "Región de Los Lagos",
+			"comunas": ["Puerto Montt", "Calbuco", "Cochamó", "Fresia", "FruVllar", "Los Muermos", "Llanquihue", "Maullín", "Puerto Varas", "Castro", "Ancud", "Chonchi", "Curaco de Vélez", "Dalcahue", "Puqueldón", "Queilén", "Quellón", "Quemchi", "Quinchao", "Osorno", "Puerto Octay", "Purranque", "Puyehue", "Río Negro", "San Juan de la Costa", "San Pablo", "Chaitén", "Futaleufú", "Hualaihué", "Palena"]
+	},
+		{
+			"NombreRegion": "Región Aisén del Gral. Carlos Ibáñez del Campo",
+			"comunas": ["Coihaique", "Lago Verde", "Aisén", "Cisnes", "Guaitecas", "Cochrane", "O’Higgins", "Tortel", "Chile Chico", "Río Ibáñez"]
+	},
+		{
+			"NombreRegion": "Región de Magallanes y de la AntárVca Chilena",
+			"comunas": ["Punta Arenas", "Laguna Blanca", "Río Verde", "San Gregorio", "Cabo de Hornos (Ex Navarino)", "AntárVca", "Porvenir", "Primavera", "Timaukel", "Natales", "Torres del Paine"]
+	},
+		{
+			"NombreRegion": "Región Metropolitana de Santiago",
+			"comunas": ["Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "TilVl", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"]
+	}]
+}
+
+
+jQuery(document).ready(function () {
+
+	var iRegion = 0;
+	var htmlRegion = '<option value="sin-region">Seleccione región</option><option value="sin-region">--</option>';
+	var htmlComunas = '<option value="sin-region">Seleccione comuna</option><option value="sin-region">--</option>';
+
+	jQuery.each(RegionesYcomunas.regiones, function () {
+		htmlRegion = htmlRegion + '<option value="' + RegionesYcomunas.regiones[iRegion].NombreRegion + '">' + RegionesYcomunas.regiones[iRegion].NombreRegion + '</option>';
+		iRegion++;
+	});
+
+	jQuery('#regiones').html(htmlRegion);
+	jQuery('#comunas').html(htmlComunas);
+
+	jQuery('#regiones').change(function () {
+		var iRegiones = 0;
+		var valorRegion = jQuery(this).val();
+		var htmlComuna = '<option value="sin-comuna">Seleccione comuna</option><option value="sin-comuna">--</option>';
+		jQuery.each(RegionesYcomunas.regiones, function () {
+			if (RegionesYcomunas.regiones[iRegiones].NombreRegion == valorRegion) {
+				var iComunas = 0;
+				jQuery.each(RegionesYcomunas.regiones[iRegiones].comunas, function () {
+					htmlComuna = htmlComuna + '<option value="' + RegionesYcomunas.regiones[iRegiones].comunas[iComunas] + '">' + RegionesYcomunas.regiones[iRegiones].comunas[iComunas] + '</option>';
+					iComunas++;
+				});
+			}
+			iRegiones++;
+		});
+		jQuery('#comunas').html(htmlComuna);
+	});
+	jQuery('#comunas').change(function () {
+		if (jQuery(this).val() == 'sin-region') {
+			alert('selecciones Región');
+		} else if (jQuery(this).val() == 'sin-comuna') {
+			alert('selecciones Comuna');
+		}
+	});
+	jQuery('#regiones').change(function () {
+		if (jQuery(this).val() == 'sin-region') {
+			alert('selecciones Región');
+		}
+	});
+
+});
